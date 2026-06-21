df62c53bf61cf6618a26c27ce64d3323d6faac98
Dipali Tungar <tungardipali6113@gmail.com>
2026-06-21 16:15:39 +0530

    add java programs

---
+++ ch1_variables&datatypes/ch_1practQ5.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class ch_1practQ5 {
+    public static void main(String[] args) {
+        System.out.println("Enter your number");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+
+    }
+}
+
---
+++ ch1_variables&datatypes/literls.java
@@ -0,0 +1,9 @@
+package com.company;
+public class  literls{
+    public static void main(String[] args) {
+//Scanner sc=new Scanner(System.in);
+     int a=23;
+     float b=58;
+     System.out.println(a);
+    }
+}
