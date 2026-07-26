2d9313eded308521b65e8ad64a0d6f15d5150ecc
Dipali Tungar <tungardipali6113@gmail.com>
2026-07-26 18:47:12 +0530

    Delete ch13_Multithreading/Circle.java

--- ch13_Multithreading/Circle.java
+++
@@ -1,9 +0,0 @@
-package ch13_Multithreading;
-public class Circle extends Shape{
-     Circle(int dim1){
-        super(dim1, -1);
-     }
-     public int area(){
-        return Math.PI * this.dim1 * this.dim1;
-     }
-}
