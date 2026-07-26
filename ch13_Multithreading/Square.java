0327591aec4101ac643dde0d5fac3a0db97a6e8c
Dipali Tungar <tungardipali6113@gmail.com>
2026-07-26 18:55:37 +0530

    Delete ch13_Multithreading/Square.java

--- ch13_Multithreading/Square.java
+++
@@ -1,9 +0,0 @@
-package ch13_Multithreading;
-public class Square extends Shape{
-     Square(int dim1){
-        super(dim1, -1);
-     }
-     public int area(){
-        return this.dim1 * this.dim1;
-     }
-}
